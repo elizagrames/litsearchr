cf13d16f8d851d42b27856033b53ac5e2ad2f0f1
Eliza Grames <eliza.grames@uconn.edu>
2018-12-20 14:07:06 -0600

    added directory of examples for man

---
+++ examples/language_graphs.R
@@ -0,0 +1,1 @@
+language_graphs(lang_data=get_language_data(key_topics=c("biology", "conservation", "ecology")), no_return=15, key_topics=c("biology", "conservation", "ecology"))
---
+++ examples/write_search.R
@@ -0,0 +1,1 @@
+write_search(groupdata=list(c("black-backed woodpecker", "picoides arcticus"), c("post-fire forest", "time since fire")), languages="English", exactphrase=TRUE, stemming=TRUE, verbose=TRUE, writesearch=FALSE)
